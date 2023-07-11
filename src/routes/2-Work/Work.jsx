fc058e933d550809c72896e784146e28e1367fdb
Myles DeBoer <myles.deboer@gmail.com>
2023-07-11 09:29:24 -0700

    work page added

--- src/routes/2-Work/Work.jsx
+++ src/routes/2-Work/Work.jsx
@@ -1,12 +1,48 @@
-  // REACT
-import react from 'react'
+  // REACT:
+import { Link } from 'react-router-dom'
 
-// SERVICES
-export default function Services() {
+// COMPONENTS:
+import Footer from '../../components/5-Footer/Footer'
+
+// CSS:
+import styles from './Work.module.css'
+
+// IMGS:
+import banner from '../../imgs/banner3TemplateCBM.jpg'
+import photo from '../../imgs/gearImg.png'
+
+// SERVICES:
+export default function Work() {
+  window.scrollTo(0,0)
+
   return (
     <>
 
-      <h1>Services</h1>
+      <img className={styles.banner} src={banner} alt="" srcset="" />
+
+      <main className={styles.workMain}>
+
+        <div className={styles.workHeaderBox}>
+          <h1>Work</h1>
+        </div>
+
+        <div className={styles.workButtonBox}>
+          <Link to='/socialmedia'><button>Social Media</button></Link>
+          <Link to='/photography'><button>Photography</button></Link>
+          <Link to='/video'><button>Video</button></Link>
+        </div>
+
+        <div className={styles.workContent}>
+          <img src={photo} alt="" srcset="" />
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, iure delectus. Ratione sint molestiae accusamus minima ab, voluptatem doloribus asperiores accusantium at, dolor hic. Ipsa natus earum quam ipsum eum!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, iure delectus. Ratione sint molestiae accusamus minima ab, voluptatem doloribus asperiores accusantium at, dolor hic. Ipsa natus earum quam ipsum eum!</p>
+          </div>
+        </div>
+
+      </main>
+
+      <Footer />
 
 
     </>
